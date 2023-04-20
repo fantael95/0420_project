8b99e8068a4ce96402cc755a0ff05de590854a5e
fanta95 <fantael95@gmail.com>
2023-04-20 14:18:50 +0900

    초기작업

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,20 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
-
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True) # 이메일 필드 추가
+    
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-
-
+        fields = ('username', 'email', 'password1', 'password2') # 필드 순서 변경
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
--- articles/models.py
+++ articles/models.py
@@ -8,6 +8,7 @@ class Article(models.Model):
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
     title = models.CharField(max_length=10)
     content = models.TextField()
+    image = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
